92fae73d88b199a9ee13c31b860e7bd74bcc53e7
NikotoKoto <floris.nicolas77@gmail.com>
2025-03-05 11:10:42 +0100

    First Component add to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { DymaComponent } from "./dyma/dyma.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [DymaComponent]
 })
 export class AppComponent {
   title = 'ProjectAngular';
