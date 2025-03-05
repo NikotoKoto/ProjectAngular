689e432cb9b24a73d49e3cebda94fa24a099b8c9
NikotoKoto <floris.nicolas77@gmail.com>
2025-03-05 11:07:09 +0100

    first component create

---
+++ src/app/dyma/dyma.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dyma',
+  standalone: true,
+  imports: [],
+  templateUrl: './dyma.component.html',
+  styleUrl: './dyma.component.css'
+})
+export class DymaComponent {
+
+}
